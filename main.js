80ab9b758aa38993278617b90ab4de1da05d36e2
Ilya Bazhenov <31971067+bazhil@users.noreply.github.com>
2018-04-23 19:02:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,84 +1,102 @@
 'use strict'
 
-//task 1
-function guaranteePrice(termOfGuarantee){
-  var priceOfOneYearGuarantee = 1250;
-  var priceOfTwoYearGuarantee = 2300;
-  
-  if (termOfGuarantee === 1) {
-    return priceOfOneYearGuarantee;
-  } else if (termOfGuarantee === 2) {
-    return priceOfTwoYearGuarantee;
-  } else {
-    priceOfGuarantee = 0;
-    return priceOfGuarantee;
+var positions = [
+  {
+    title: 'Телепорт бытовой VZHIH-101',
+    producer: {
+      name: 'Рязанский телепортостроительный завод',
+      deferPeriod: 10,
+      lot: 3
+    },
+    price: 10000
+  },
+  {
+    title: 'Ховерборд Mattel 2016',
+    producer: {
+      name: 'Волжский Ховерборд Завод',
+      deferPeriod: 24,
+      lot: 14
+    },
+    price: 9200
+  },
+  {
+    title: 'Меч световой FORCE (синий луч)',
+    producer: {
+      name: 'Тульский оружейный комбинат',
+      deferPeriod: 5,
+      lot: 1
+    },
+    price: 57000
   }
-} 
+];
 
-var price = guaranteePrice(1);
-console.log(`Дополнительное гарантийное обслуживание: ${price} Q`);
+// task 1
+var positionNumber = 1;
+var quantityItems = 561;
+
+function lotCalculator(arr, quantityItems){
+  var position = arr;
+  var minimumQuantityOfLots = Math.ceil(quantityItems / position.producer.lot);
+  var totalPrice = minimumQuantityOfLots * position.producer.lot * position.price;
+  var information = {};
+  information.lots = minimumQuantityOfLots;
+  information.total = totalPrice;
+  return information; 
+}
+var calculator = lotCalculator(positions[1], 15);
+console.log(calculator);
+
+var calculator2 = lotCalculator(positions[2], 1);
+console.log(calculator2);
 
 //task 2
-function engraving(engravingText) {
-  if (engravingText === ''){
-    return 0;
-  } else if (engravingText === undefined && typeof(engravingText) === 'number'){
-    return 0;
-  } else if (typeof(engravingText) === 'string'){
-  var priceOfWord = 20;
-  var words = engravingText.split(' ');
-  var wordsCount = words.length;
-  var engravingPrice = priceOfWord * wordsCount;
-  return engravingPrice;
-  } 
+const deferedPayments = [];
+var position = 1; 
+
+function deferPay(arr, totalSum, shippingDate){
+  
+  const paymentDate = new Date(shippingDate.getTime());
+  paymentDate.setDate(shippingDate.getDate() + producer.deferPeriod);
+  var paymentInformation = {};
+  paymentInformation.producer = producer;
+  paymentInformation.amount = totalSum;
+  paymentInformation.paymentDate = paymentDate;
+  deferedPayments.push(paymentInformation);
 }
 
-var engravingResult = engraving('best of the best of the best');
-console.log(`Подарочная упаковка и гравировка: ${engravingResult} Q`);
+const producer = {
+  name: 'Рязанский телепортостроительный завод',
+  deferPeriod: 10
+};
+
+deferPay(producer, 7200, new Date(2030, 4 - 1, 10));
+
+console.log(deferedPayments.length); 
+console.log(deferedPayments[0].producer.name); 
+console.log(deferedPayments[0].amount); 
+console.log(deferedPayments[0].paymentDate); 
+
+
 
 //task 3
-var needShipping = true;
-var shippingAdress = 'Nabu';
-
-
-function shipping(needShipping, shippingAdress) {
-var shippingPrice;  
-
-if (needShipping === true){
-  switch (shippingAdress){
-    case 'Earth':
-      return 550;
-    case 'Moon':
-      return 550;
-    case 'Mars':
-      return 550;  
-    case 'Mercury':
-      return 550;   
-    case 'Andromeda':
-      return 550;    
-    case 'Cassiopeia':
-      return 550;   
-    case 'Tatooine':
-      return 550;     
-    case 'Nabu':
-      return 550;  
-    default:
-      return NaN;
-  }
-
-} else if(shippingAdress === undefined || needShipping === undefined){
-  return NaN;
-} else {
-  return 0;
-}
+function loadCurrencyJSON(){
+  return '{"AUD":44.95,"AZN":33.73,"GBP":73.42,"AMD":0.12,"BYN":30.96,"BGN":32.01,"BRL":18.8,"HUF":0.2,"DKK":8.42,"USD":58.85,"EUR":62.68,"INR":0.88,"KZT":0.18,"CAD":44.74,"KGS":0.85,"CNY":8.55,"MDL":2.94,"NOK":7.02,"PLN":14.55,"RON":13.92,"ZZZ":79.91,"SGD":41.36,"TJS":7.43,"TRY":15.97,"TMT":16.84,"UZS":0.02,"UAH":2.16,"CZK":2.32,"SEK":6.6,"CHF":58.69,"ZAR":4.4,"KRW":0.05,"JPY":0.52}';
 }
 
-var shippingResult = shipping(true, 'Moon');
+var amount = 516;
+var from = "EUR";
+var to = "USD";
 
-if (shippingResult > 0){
-  console.log(`Стоимость доставки: ${shippingResult} Q`);
-} else if (shippingResult === 0){
-  console.log('Доставка не требуется');
-} else{
-  console.log('Ошибка при расчете стоимости доставки');
+function convertCurrency(amount, from, to){
+    try{
+      var currency = JSON.parse(loadCurrencyJSON());
+      var courseFrom = currency[from];
+      var courseTo = currency[to];
+      var currencyChange = ((amount * courseFrom) / courseTo).toFixed(2);
+      return currencyChange;
+    } catch(e){
+      console.error(e.name, e.message);
+    }
 }
+
+convertCurrency(500, "EUR", "USD");
